4f9dced2bd08353b188d0ebf9c96ff1d7a7896ec
dziobadzzA <61158312+dziobadzzA@users.noreply.github.com>
2020-04-13 00:03:55 +0300

    fibon.kt

--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -2,8 +2,29 @@ package subtask6
 
 class Fibonacci {
 
-    // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
+
+        var result: IntArray = intArrayOf(0,0,0)
+        var fibon: MutableList<Int> = mutableListOf(0, 1)
+        var fibon_index = 0
+
+        var ok = true
+
+        while (ok)
+        {
+            if ((fibon[fibon_index] * fibon[fibon_index + 1]) < n)
+            {
+                fibon.add(fibon[fibon_index] + fibon[fibon_index+1])
+                fibon_index++
+            }
+            else {ok = false}
+        }
+
+        result[0] = fibon[fibon_index]
+        result[1] = fibon[fibon_index+1]
+
+        if(result[0] * result[1] == n) { result[2] = 1 }
+
+        return result
     }
 }
