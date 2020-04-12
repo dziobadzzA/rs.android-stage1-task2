795e25b2ffee039844675a55c046235ef6ae19f7
dziobadzzA <61158312+dziobadzzA@users.noreply.github.com>
2020-04-13 00:01:02 +0300

    update subtask3

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -2,8 +2,43 @@ package subtask3
 
 class Abbreviation {
 
-    // TODO: Complete the following function
-    fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+    fun abbreviationFromA(a: String, b: String): String
+    {
+        var result = "NO"
+
+        var I = -1
+        var index_I = -1
+        var length_I = 0
+
+        var bufic = a.toUpperCase().toCharArray()
+        var bufic_b = b.toCharArray()
+
+        for (i in 0.. bufic.size-1)
+        {
+            I = -1
+
+            for (j in 0.. bufic_b.size-1)
+            {
+                if ( bufic[i] == bufic_b[j])
+                {
+                    I = j;
+                    break;
+                }
+            }
+
+            if ((I > -1) and (I > index_I))
+            {
+                index_I = I
+                length_I++
+
+                if (length_I == b.length) {break;}
+
+            }
+
+        }
+
+        if (length_I == b.length) {result = "YES"}
+
+        return result
     }
 }
